74d5caa4dbcf487c0d5cd57fe8adb67008dfc9f7
lahcenedaif <dev.lah.eda@gmail.com>
2025-08-08 15:54:08 +0200

    Fix LTR in firfox

--- src/utils/LanguageUtils.ts
+++ src/utils/LanguageUtils.ts
@@ -1,21 +1,23 @@
 interface LocaleWithTextInfo extends Intl.Locale {
-  textInfo?: { direction: "ltr" | "rtl" };
+  textInfo: { direction: "ltr" | "rtl" };
 }
-
+const supportedRtlLangueges = ["ar", "he"];
 export const isRTL = (lang: string) => {
   try {
     const locale = new Intl.Locale(lang) as LocaleWithTextInfo;
-    return locale.textInfo?.direction === "rtl";
+    return locale.textInfo.direction === "rtl";
   } catch {
-    return false;
+    return supportedRtlLangueges.includes(lang);
   }
 };
-export const getDir = (lang?: string): "ltr" | "rtl" => {
+export const getDir = (lang: string): "ltr" | "rtl" => {
   try {
-    const locale = new Intl.Locale(lang ?? "en") as LocaleWithTextInfo;
-    return locale.textInfo?.direction === "rtl" ? "rtl" : "ltr";
+    const locale = new Intl.Locale(lang) as LocaleWithTextInfo;
+    // I had compatibity issues with this only chromium based browsers support it for now
+    return locale.textInfo.direction === "rtl" ? "rtl" : "ltr";
   } catch {
-    return "ltr";
+    // I added this beacause fireFox does not support locale.textInfo for now
+    return supportedRtlLangueges.includes(lang) ? "rtl" : "ltr";
   }
 };
 export const setDir = (dir: string): void => {
